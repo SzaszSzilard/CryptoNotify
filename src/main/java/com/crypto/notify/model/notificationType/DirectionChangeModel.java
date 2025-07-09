60e9bc5d46a14f23d01e3d0ed44d6c2fa128b181
Szilard Szasz <szasz.szilard7@gmail.com>
2025-07-09 22:07:56 +0300

    DirectionChangeModel notification function defined

--- src/main/java/com/crypto/notify/model/notificationType/DirectionChangeModel.java
+++ src/main/java/com/crypto/notify/model/notificationType/DirectionChangeModel.java
@@ -4,6 +4,7 @@ import com.crypto.notify.constants.NotificationTypeConstants;
 import com.crypto.notify.model.notificationBase.NonTargetNotificationModel;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DirectionChangeModel extends NonTargetNotificationModel {
 
@@ -12,14 +13,36 @@ public class DirectionChangeModel extends NonTargetNotificationModel {
         this.type = NotificationTypeConstants.N_DIRECTION_CHANGE;
     }
 
+    private boolean consistent(long trend, long windowSize) {
+        return trend >= windowSize * 0.75;
+    }
+
+    private int getTrend(List<Double> prices) {
+        long rising = IntStream.range(1, prices.size())
+                .filter(i -> prices.get(i) > prices.get(i - 1))
+                .count();
+
+        long falling = IntStream.range(1, prices.size())
+                .filter(i -> prices.get(i) < prices.get(i - 1))
+                .count();
+
+        if (consistent(rising, prices.size())) {
+            return 1; // Rising trend
+        } else if (consistent(falling, prices.size())) {
+            return -1; // Falling trend
+        } else {
+            return 0; // No clear trend
+        }
+    }
+
     @Override
     public double shouldNotify(List<Double> prices) {
-//        previousWindow = [102, 101, 100]
-//        currentWindow = [101, 103, 105]
-//        prevTrend = getTrend(previous_window)
-//        currTrend = getTrend(current_window)
-//
-//        return prev_trend != curr_trend && curr_trend != "flat";
+        int prevTrend = getTrend( prices.subList( 0, prices.size()/2 ));
+        int currTrend = getTrend( prices.subList( prices.size()/2, prices.size() ));
+
+        if (prevTrend != currTrend && currTrend != 0) {
+            return currTrend;
+        }
 
         return 0;
     }
